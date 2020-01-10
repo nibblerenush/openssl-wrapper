9a9217d894c14ec99808f5b5b613f4cacdc61276
kshakhayev <kaymran.shakhayev@stoloto.ru>
2020-01-10 16:21:53 +0300

    Added: README.md text, full example code

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # openssl-wrapper
+This is the object oriented wrapper library under [OpenSSL](https://www.openssl.org/) libcrypto library.
+
+## Dependencies
+* openssl >= 1.0.2
+
+## Build
+```cmake <sources folder>```
+
+## Using
+It's necessary to call method **Initialize** before using other classes and methods.
+
+```C++
+Initialize();
+
+RsaKey rsaKey;
+rsaKey.GenerateKey();
+rsaKey.SetPlaintext(plaintext);
+rsaKey.Encrypt()
+std::cout << "plaintext: " << BaseFunctions::GetAsciiString(plaintext) << '\n'
+  << "ciphertext: " << BaseFunctions::GetHexString(rsaKey.GetCiphertext()) << std::endl;
+```
--- example/main.cpp
+++ example/main.cpp
@@ -1,12 +1,21 @@
 #include <cstring>
+#include <memory>
 #include <iostream>
 
-#include "BaseFunctions.h"
-#include "Initializer.h"
-#include "Digest.h"
-#include "Hmac.h"
+#include "params/DhParams.h"
+#include "params/DsaParams.h"
+#include "params/EcParams.h"
+#include "params/Parameters.h"
 
+#include "BaseFunctions.h"
 #include "Cipher.h"
+#include "Digest.h"
+#include "DigitalSignature.h"
+#include "Hmac.h"
+#include "Initializer.h"
+#include "Key.h"
+#include "KeyAgreement.h"
+#include "RsaKey.h"
 
 using namespace openssl_wrapper;
 
@@ -14,7 +23,7 @@ int main(int argc, char ** argv)
 {
   if (argc != 2)
   {
-    std::cerr << "Usage: " << argv[0] << " <type of example>" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " <operation type>" << std::endl;
     return EXIT_FAILURE;
   }
 
@@ -24,6 +33,7 @@ int main(int argc, char ** argv)
   {
     bytes_t plaintext = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
 
+    // Symmetric encryption
     if (std::strcmp(argv[1], "Cipher") == 0)
     {
       Cipher cipher("aes-128-cbc");
@@ -39,16 +49,106 @@ int main(int argc, char ** argv)
         << "ciphertext: " << BaseFunctions::GetHexString(cipher.GetCiphertext()) << std::endl;
     }
 
-    //auto data = BaseFunctions::GetFileData("msg.txt");
-    //auto result = Digest::GetHash("SHA1", data);
-    /*auto result = Hmac::GetMac("SHA1", data, {'1', '2', '3', '4'});
-    std::cout << BaseFunctions::GetHexString(result) << std::endl;*/
+    // Diffie Hellman
+    else if (std::strcmp(argv[1], "KeyAgreement") == 0)
+    {
+      std::unique_ptr<Parameters> dhParams(new DhParams());
+      dhParams->GenerateParameters();
+      dhParams->WriteParametersToFile("DhParams.pem");
+
+      std::unique_ptr<Parameters> ecParams(new EcParams());
+      ecParams->GenerateParameters();
+      ecParams->WriteParametersToFile("EcParams.pem");
+
+      Key dhKey1;
+      dhKey1.GenerateKey(dhParams.get());
+      dhKey1.WritePrivateKeyToFile("dhKey1.pem", "aes-128-cbc", "1234");
+
+      Key dhKey2;
+      dhKey2.GenerateKey(dhParams.get());
+      dhKey2.WritePrivateKeyToFile("dhKey2.pem", "aes-128-cbc", "1234");
+
+      Key ecKey1;
+      ecKey1.GenerateKey(ecParams.get());
+      ecKey1.WritePrivateKeyToFile("ecKey1.pem", "aes-128-cbc", "1234");
+
+      Key ecKey2;
+      ecKey2.GenerateKey(ecParams.get());
+      ecKey2.WritePrivateKeyToFile("ecKey2.pem", "aes-128-cbc", "1234");
+
+      bytes_t dhKeyAgreement = KeyAgreement::KeyExchange(dhKey1, dhKey2);
+      bytes_t ecKeyAgreement = KeyAgreement::KeyExchange(ecKey1, ecKey2);
+
+      std::cout << "DH common key: " << BaseFunctions::GetHexString(dhKeyAgreement) << '\n'
+        << "EC common key: " << BaseFunctions::GetHexString(ecKeyAgreement) << std::endl;
+    }
+
+    // RSA encryption
+    else if (std::strcmp(argv[1], "RsaEncrypt") == 0)
+    {
+      RsaKey rsaKey;
+      rsaKey.GenerateKey();
+      rsaKey.WritePrivateKeyToFile("rsaEncryptKey.pem", "aes-128-cbc", "1234");
+
+      rsaKey.SetPlaintext(plaintext);
+      rsaKey.Encrypt();
+      
+      std::cout << "plaintext: " << BaseFunctions::GetAsciiString(plaintext) << '\n'
+        << "ciphertext: " << BaseFunctions::GetHexString(rsaKey.GetCiphertext()) << std::endl;
+    }
+
+    // Digital signature
+    else if (std::strcmp(argv[1], "DigitalSignature") == 0)
+    {
+      RsaKey rsaKey;
+      rsaKey.GenerateKey();
+      rsaKey.WritePrivateKeyToFile("rsaSignKey.pem", "aes-128-cbc", "1234");
+
+      std::unique_ptr<Parameters> dsaParams(new DsaParams);
+      dsaParams->GenerateParameters();
+      dsaParams->WriteParametersToFile("dsaParams.pem");
+
+      Key dsaKey;
+      dsaKey.GenerateKey(dsaParams.get());
+      dsaKey.WritePrivateKeyToFile("dsaKey.pem", "aes-128-cbc", "1234");
+
+      bytes_t rsaSignature = DigitalSignature::Sign(rsaKey, "SHA256", plaintext);
+      bytes_t dsaSignature = DigitalSignature::Sign(dsaKey, "SHA256", plaintext);
+
+      std::cout << "RSA signature: " << BaseFunctions::GetHexString(rsaSignature) << '\n'
+        << "Dsa signature: " << BaseFunctions::GetHexString(dsaSignature) << std::endl;
+    }
+
+    // Digest
+    else if (std::strcmp(argv[1], "Digest") == 0)
+    {
+      bytes_t digest = Digest::GetHash("SHA1", plaintext);
+
+      std::cout << "plaintext: " << BaseFunctions::GetAsciiString(plaintext) << '\n'
+        << "digest: " << BaseFunctions::GetHexString(digest) << std::endl;
+    }
+
+    // Hmac
+    else if (std::strcmp(argv[1], "Hmac") == 0)
+    {
+      bytes_t hmac = Hmac::GetMac("SHA1", plaintext, {'1', '2', '3', '4'});
+      
+      std::cout << "plaintext: " << BaseFunctions::GetAsciiString(plaintext) << '\n'
+        << "hmac: " << BaseFunctions::GetHexString(hmac) << std::endl;
+    }
+
+    // Invalid
+    else
+    {
+      std::cerr << "Invalid operation type!" << std::endl;
+      return EXIT_FAILURE;
+    }
   }
   catch (const WrapperException & ex)
   {
     std::cerr << "Error: " << ex.what() << std::endl;
     return EXIT_FAILURE;
   }
-
+  
   return EXIT_SUCCESS;
 }
